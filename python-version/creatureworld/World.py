c0cc9954e7d8982995f4f63295faeda2acdc9b68
Erik Edin <erik.a.edin@gmail.com>
2022-05-02 23:17:33 +0200

    Added initial version of surroundings function

--- python-version/creatureworld/World.py
+++ python-version/creatureworld/World.py
@@ -36,7 +36,7 @@ class World:
         self._grid.iloc[coordinate[0], coordinate[1]] = 'F'
 
     def placeCreature(self, coordinate, creature):
-        self._grid.iloc[coordinate[0], coordinate[1]] = creature
+        self._grid.iloc[coordinate[0], coordinate[1]] = 'c'#creature
 
     def createFoodSupply(self, percentFood: float) -> None:
         """ Fills the world randomly with food. 0 --> no food, 1 --> food on each part """
@@ -66,6 +66,63 @@ class World:
             creatureMove = self._creatures_list[n].make_move([0,1,2])
             print(creatureMove)
 
+    def creatureSurroundings(self, position = (0,6), vision=1):
+        '''Function that returns the suroundings of a creature'''
+        # Wall detection
+        wall = -1
+        # Behöver generalisera för att funka med vision som inte är 1
+        # övre/undre raden börjar på 3 vision ett och växer med +2 för varje vision
+        # höger/vänster kolumn börjar på 1 vision ett, växer med +2 för varje vision
+        # Bugg i nuvarande version, hanterar inte hörnen när man slår i två bounds
+
+        upper = False
+        right = False
+        bottom = False
+        left = False
+
+        # Detect walls
+        if position[0] - 1 <= -1: # Upper wall
+            upper = True
+        if position[1] +1 >= self.w_dim[1]: # Right wall
+            right = True
+        if position[0] + 1 >= self.w_dim[0]:
+            bottom = True
+        if position[1] - 1 <= -1:
+            left = True
+        
+        # Detect upper wall row-1 out-of-bounds
+        # Lägg till hantering för kombinationer i hörnen
+        if upper: # Upper wall
+            ser1 = pd.Series([wall for i in range(3)])
+        else:
+            ser1 = pd.Series(self._grid.iloc[position[0]-1][position[1]-1:position[1]+1+1])
+
+        # Detect right wall column+1 out-of-bounds
+        if right:
+            ser2 = pd.Series([wall for i in range(1)])
+        else:
+            ser2 = pd.Series(self._grid.iloc[position[0]][position[1]+1])
+        
+        # Detect lower wall row+1 out-of-bounds
+        if bottom:
+            ser3 = pd.Series([wall for i in range(3)])
+        else:
+            ser3 = pd.Series(self._grid.iloc[position[0]+1][position[1]+1:position[1]-1-1:-1])
+        
+        # Detect left wall column-1 out-of-bounds
+        if left:
+            ser4 = pd.Series([wall for i in range(1)])
+        else:
+            ser4 = pd.Series(self._grid.iloc[position[0]][position[1]-1])        
+            
+        surroundings= pd.concat([ser1,ser2,ser3,ser4])
+        dummies = pd.get_dummies(surroundings) # drop_first funkar inte i det här läget
+        #print("Ser1: \n ",ser1, "ser2: \n ",ser2,"ser3: \n", ser3, "ser4: \n", ser4)
+        print(surroundings)
+        print(dummies)
+        print(dummies.columns)
+
+
 
 def main():
     
@@ -84,6 +141,8 @@ def main():
     print(my_world)
 
     my_world.iterateWorld()
+
+    my_world.creatureSurroundings()
 
 
 if __name__ == "__main__":
